a09e68e305c7c250b9087bef9e63e88a2f8e85fe
rakhmanhanna <rakhmanhanna@gmail.com>
2022-02-17 12:11:41 +0300

    Arrays

---
+++ 3.Массивы.js
@@ -0,0 +1,27 @@
+// минимум массива
+var M = [7, 66, 55, 4, 88, 1, 8, 99, 3];
+
+min = M[0]; // Минимум
+// minInd = 0; // Индекс min
+
+for (i = 1; i < M.length; i++) {
+  if (M[i] < min) {
+    min = M[i];
+    // minInd = i;
+  } // if
+} // for
+console.log('Минимум: ' + min);
+// console.log('Индекс: ' + minInd);
+
+// max
+var M = [7, 66, 55, 4, 88, 1, 8, 99, 3];
+
+max = M[0];
+
+for (i = 1; i < M.length; i++) {
+  if (M[i] > max) {
+    max = M[i];
+  }
+  //   console.log(M[i]);
+}
+console.log('max: ' + max);
