c1fd9bc0b6445e7b843ba9e61d608edd0464072b
rakhmanhanna <rakhmanhanna@gmail.com>
2022-02-26 14:09:15 +0300

    zamykanie

--- 14.Замыкание.js
+++ 14.Замыкание.js
@@ -4,3 +4,33 @@ function calculateRectangleArea(length, width) {
 
 let roomArea = calculateRectangleArea(10, 10);
 console.log(roomArea);
+
+//
+function youSayGoodBye() {
+  console.log('Good bye');
+  function andISayHello() {
+    console.log('Hello');
+  }
+
+  return andISayHello;
+}
+
+//
+function stopWatch() {
+  let startTime = Date.now();
+
+  function getDelay() {
+    let elapsedTime = Date.now() - startTime;
+    console.log(elapsedTime);
+  }
+
+  return getDelay;
+}
+
+let timer = stopWatch();
+
+for (let i = 0; i < 1000000; i++) {
+  let foo = Math.random() * 10000;
+}
+
+timer();
