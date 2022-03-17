f8c007030f2469b9a1562e8c03c5eacf23a40f88
rakhmanhanna <rakhmanhanna@gmail.com>
2022-03-17 11:35:36 +0300

    arrays

--- 3.Массивы.js
+++ 3.Массивы.js
@@ -26,3 +26,35 @@ for (i = 1; i < M.length; i++) {
 }
 console.log('max: ' + max);
 
+//
+let names = ['anna', 'alex', 'egor', 'liza'];
+
+let newNames = [];
+
+for (let i = 0; i < names.length; i++) {
+  let name = names[i];
+  let firstLetter = name.charAt(0).toLocaleUpperCase();
+  newNames.push(firstLetter + name.slice(1));
+}
+
+console.log(newNames);
+
+//
+let names = ['anna', 'alex', 'egor', 'liza'];
+
+function capitalizeItUp(item) {
+  let firstLetter = item.charAt(0).toUpperCase();
+  return firstLetter + item.slice(1);
+}
+
+let newNames = names.map(capitalizeItUp);
+console.log(newNames);
+
+//
+let numbers = [1, 2, 3, 4, 5];
+
+let evenNumbers = numbers.filter(function (item) {
+  return item % 2 == 0;
+});
+
+console.log(evenNumbers);
